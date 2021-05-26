c878ee91668c178d44e551d86b39dbdd6644636a
Michaelsp11 <michaelsalgadop11@gmail.com>
2021-05-26 21:11:59 +0200

    Script también convierte camelCase en snakecase

--- js/main.js
+++ js/main.js
@@ -1,4 +1,10 @@
-let snakeCase = "Hola_Que_tal";
+/*
+Punto 1
+Crea un script JS que declare una variable y asígnale un string que sea una frase escrita en snakecase.
+
+Haz que el script convierta esa frase en camelCase (con la primera en minúsculas) y la imprima por consola.
+*/
+let snakeCase = "hola_amigos_programadores";
 snakeCase = snakeCase.split("_");
 console.log(toCamelCase(snakeCase));
 
@@ -13,4 +19,21 @@ function toCamelCase(arrayString) {
         }
     }
     return camelCase;
+}
+/*
+Punto 2
+Crea un script JS que declare una variable y asígnale un string que sea una frase escrita en camelCase (con la primera en minúsculas).
+
+Haz que el script convierta esa frase en snakecase y la imprima por consola.
+*/
+let camelCase = "holaAmigosProgramadores";
+console.log(toSnakeCase(camelCase));
+
+function toSnakeCase(frase) {
+    for (let i = 0; i < frase.length; i++) {
+        if (frase.charAt(i) === frase.charAt(i).toUpperCase()) {
+            frase = frase.replace(frase.charAt(i), '_' + frase.charAt(i).toLowerCase());
+        }
+    }
+    return frase;
 }
